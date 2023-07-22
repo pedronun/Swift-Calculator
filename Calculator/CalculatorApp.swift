6ee46211fa78991a8ec71d733f7bccba83438a12
Pedro Lucas <pedro.lucas@econverse.com.br>
2023-07-22 20:16:01 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Econverse on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
